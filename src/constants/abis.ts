7cf689bd182258fea96dc752e11fc1b98857a605
WillBillyGit <Bullerwill@gmail.com>
2026-05-02 19:54:44 +1200

    Update abis.ts

--- src/constants/abis.ts
+++ src/constants/abis.ts
@@ -564,7 +564,7 @@ export const VAULT_ABI = [
     "stateMutability": "view",
     "type": "function"
   }
-] as const;
+]as const;
 
 export const ERC20_ABI = [
     { "constant": true, "inputs": [{ "name": "_owner", "type": "address" }], "name": "balanceOf", "outputs": [{ "name": "balance", "type": "uint256" }], "type": "function" },
